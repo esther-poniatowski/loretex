3fd0fd3a2ccaa09cd77f8f2d5c7078a64b318341
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-06-07 19:49:30 +0200

    fix(src): Delegate version import to package metadata

--- src/loretex/__init__.py
+++ src/loretex/__init__.py
@@ -14,7 +14,8 @@ parsers:
 utils:
     Utility functions for I/O operations and YAML handling.
 """
+from importlib.metadata import version
 
-__version__ = "0.1.0"
+__version__ = version(__package__)
 __author__ = "Esther Poniatowski"
 __email__ = "esther.poniatowski@ens.psl.eu"
