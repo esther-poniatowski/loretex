077b055caee3e4c80216f0482f46235cbc010cfe
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-06-08 12:55:02 +0200

    feat(__main__): Add entry point

--- src/loretex/__main__.py
+++ src/loretex/__main__.py
@@ -1,28 +1,17 @@
 """
-Package: `loretex` (`__main__.py`)
-
-Command-line entry point for the loretex package.
-
-This entry point loads a YAML specification file and performs Markdown-to-LaTeX conversion for all
-listed notes.
+Command-line entry point for the `loretex` package.
 
 Usage
 -----
-To convert Markdown notes to LaTeX files from a YAML specification file, run:
+To invoke the package::
 
-```sh
-python -m loretex --spec path/to/spec.yml
-```
+    python -m loretex
 
-Arguments
----------
---spec : Path
-    Path to the YAML specification file that defines the Markdown notes to convert.
 
 See Also
 --------
-loretex.cli: Command-line interface module for the loretex package.
+loretex.cli: Command-line interface module for the package.
 """
-from loretex.cli import app
+from .cli import app
 
 app()
